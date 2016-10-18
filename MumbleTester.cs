573c2ae8c90c385ea7a0c665312ab73cb87e6f4c
VRtube <vrtubexxx@gmail.com>
2016-10-18 16:29:48 -0700

    Added testing script

---
+++ MumbleTester.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mumble;
+
+public class MumbleTester : MonoBehaviour {
+
+    public MumbleAudioPlayer[] MumbleAudioPlayers;
+    public SendMumbleAudio AudioSender;
+
+    private MumbleClient _mumbleClient;
+    public string hostName = "1.2.3.4";
+    public int port = 64738;
+    public string _username = "SuperUser";
+    public string _password = "1passwordHere!";
+
+	void Start () {
+
+        _mumbleClient = new MumbleClient(hostName, port);
+        _mumbleClient.Connect(_username, _password);
+        
+        foreach(MumbleAudioPlayer audioPlayer in MumbleAudioPlayers)
+        {
+            audioPlayer.SetMumbleClient(_mumbleClient);
+        }
+        AudioSender.Initialize(_mumbleClient);
+	}
+	
+    void OnApplicationQuit()
+    {
+        Debug.LogWarning("Shutting down connections");
+        _mumbleClient.Close();
+    }
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            _mumbleClient.Process();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            print("Is Connected: " + _mumbleClient.ConnectionSetupFinished);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _mumbleClient.SendTextMessage("This is an example message from Unity");
+            print("Sent mumble message");
+        }
+	}
+}
