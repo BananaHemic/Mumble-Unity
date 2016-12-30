732005de3e57e1b10d05cbb317f32b52fd7db039
VRtube <vrtubexxx@gmail.com>
2016-12-30 13:09:55 -0800

    Switched audio playback to OnAudioFilterRead
    Previously we were playing audio by overloading OnAudioRead, we now
    overload OnAudioFilterRead, allowing lower latency

--- Scripts/MumbleAudioPlayer.cs
+++ Scripts/MumbleAudioPlayer.cs
@@ -4,12 +4,11 @@ using System;
 using System.Linq;
 
 namespace Mumble {
-    [RequireComponent(typeof(AudioSource))]
     public class MumbleAudioPlayer : MonoBehaviour {
 
         public float Gain = 1;
         private MumbleClient _mumbleClient;
-        private AudioSource Source;
+        //private AudioSource Source;
         private AudioClip _clip;
         private int _position = 0;
 
@@ -17,22 +16,26 @@ namespace Mumble {
         const bool isStreamingAudio = true;
 
         void Start() {
-            Source = GetComponent<AudioSource>();
-            _clip = AudioClip.Create("Received Audio", MumbleConstants.SAMPLE_RATE * SampleLengthSeconds, MumbleConstants.NUM_CHANNELS, MumbleConstants.SAMPLE_RATE, isStreamingAudio, OnAudioRead, OnAudioPositionSet);
-            Source.clip = _clip;
-            Source.Play();
+            //Source = GetComponent<AudioSource>();
+            print("outout rate " + AudioSettings.outputSampleRate);
+            //_clip = AudioClip.Create("Received Audio", MumbleConstants.SAMPLE_RATE * SampleLengthSeconds, MumbleConstants.NUM_CHANNELS, MumbleConstants.SAMPLE_RATE, isStreamingAudio, OnAudioRead, OnAudioPositionSet);
+            //Source.clip = _clip;
+            //Source.Play();
         }
         public void Initialize(MumbleClient mumbleClient)
         {
             _mumbleClient = mumbleClient;
         }
-        void OnAudioRead(float[] data)
+        void OnAudioFilterRead(float[] data, int channels)
+        //void OnAudioRead(float[] data)
         {
             //print("On audio read");
             if (_mumbleClient == null || !_mumbleClient.ConnectionSetupFinished)
                 return;
 
             _mumbleClient.LoadArrayWithVoiceData(data, 0, data.Length);
+
+            //Debug.Log("playing audio with avg: " + data.Average() + " and max " + data.Max());
             if (Gain == 1)
                 return;
 
