897f13cd7fa4cc581a2ec8e046437d48471d6fb5
Bananahemic <bananahemic@users.noreply.github.com>
2017-07-06 15:05:43 -0700

    Removed some unneeded code

--- Scripts/MumbleAudioPlayer.cs
+++ Scripts/MumbleAudioPlayer.cs
@@ -10,19 +10,13 @@ namespace Mumble {
         public float Gain = 1;
         private MumbleClient _mumbleClient;
         private UInt32 _session;
-        //private AudioSource Source;
         private AudioClip _clip;
-        private int _position = 0;
 
         const int SampleLengthSeconds = 1;
         const bool isStreamingAudio = true;
 
         void Start() {
-            //Source = GetComponent<AudioSource>();
             print("outout rate " + AudioSettings.outputSampleRate);
-            //_clip = AudioClip.Create("Received Audio", MumbleConstants.SAMPLE_RATE * SampleLengthSeconds, MumbleConstants.NUM_CHANNELS, MumbleConstants.SAMPLE_RATE, isStreamingAudio, OnAudioRead, OnAudioPositionSet);
-            //Source.clip = _clip;
-            //Source.Play();
         }
         public void Initialize(MumbleClient mumbleClient, UInt32 session)
         {
@@ -30,7 +24,6 @@ namespace Mumble {
             _session = session;
         }
         void OnAudioFilterRead(float[] data, int channels)
-        //void OnAudioRead(float[] data)
         {
             //print("On audio read");
             if (_mumbleClient == null || !_mumbleClient.ConnectionSetupFinished)
@@ -45,11 +38,6 @@ namespace Mumble {
             for (int i = 0; i < data.Length; i++)
                 data[i] = Mathf.Clamp(data[i] * Gain, -1f, 1f);
             //Debug.Log("playing audio with avg: " + data.Average() + " and max " + data.Max());
-        }
-        void OnAudioPositionSet(int position)
-        {
-            //print("Position set " + position);
-            _position = position;
         }
     }
 }
